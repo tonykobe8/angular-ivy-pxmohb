83835bfdb333f6e25c47af80c2c03ad461b4b472
tonykobe8 <tonykobe8@gmail.com>
2020-09-01 12:04:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-pxmohb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pxmohb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+
+  todolist = [{name:"noknokwanda",Sname:"hhhhh",age:"30",Tittle:"Developer"},
+  {name:"kwanda",Sname:"gggggggg",age:"40",Tittle:"Web designer"}];
+}
