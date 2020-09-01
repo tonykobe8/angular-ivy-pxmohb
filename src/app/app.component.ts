e75b38eed5ef390a088efdb2d9fdd76ae1ef8a78
tonykobe8 <tonykobe8@gmail.com>
2020-09-01 21:01:14 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ export class AppComponent  {
   name = 'Angular';
 
 
-  todolist = [{name:"noknokwanda",Sname:"hhhhh",age:"30",Tittle:"system admin",button:""},
-  {name:"kwanda",Sname:"ghhhh",age:"20",Tittle:"Web designer",button:""},
-   {name:"nda",Sname:"gg",age:"40",Tittle:"system developer",button:""},
+  todolist = [{name:"Matome",Sname:"kobe",age:"30",Tittle:"system admin",button:""},
+  {name:"hhhhnda",Sname:"ghhhh",age:"20",Tittle:"Web designer",button:""},
+   {name:"ngghdajh",Sname:"gg",age:"40",Tittle:"system developer",button:""},
     {name:"hnda",Sname:"ggggg",age:"25",Tittle:"ICT officer",button:""},
-     {name:"1mmanda",Sname:"g",age:"24",Tittle:"IT technician",button:""},
-     {name:"2mmanda",Sname:"kkkk",age:"24",Tittle:"software developer",button:""},
-     {name:"3mmanda",Sname:"nnnnn",age:"24",Tittle:"Software tester",button:""},
-     {name:"4mmanda",Sname:"ppppp",age:"24",Tittle:"IT technician",button:""},
-     {name:"5mmanda",Sname:"uuuu",age:"24",Tittle:"IT security",button:""},
+     {name:"1mmanhda",Sname:"g",age:"24",Tittle:"IT technician",button:""},
+     {name:"2mmavnda",Sname:"kkkk",age:"24",Tittle:"software developer",button:""},
+     {name:"3mmandbna",Sname:"nnnnn",age:"24",Tittle:"Software tester",button:""},
+     {name:"4mmhhhanda",Sname:"ppppp",age:"24",Tittle:"IT technician",button:""},
+     {name:"5mmahnda",Sname:"uuuu",age:"24",Tittle:"IT security",button:""},
   {name:"kkkwanda",Sname:"gjjgjjkk",age:"33",Tittle:"system analyst",button:""}];
 };
