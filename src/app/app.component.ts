06170de29fca9373156133b3690c362d8394ebb6
tonykobe8 <tonykobe8@gmail.com>
2020-09-02 15:13:23 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,13 @@ export class AppComponent  {
   name = 'Angular';
 
   
-  todolist = [{name:"Matome",Sname:"kobe",age:"30",Tittle:"system admin",button:"",image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/349353287.jpg"},
-  {name:"hhhhnda",Sname:"ghhhh",age:"20",Tittle:"Web designer",button:""},
-   {name:"ngghdajh",Sname:"gg",age:"40",Tittle:"system developer",button:"",image:"https://stackblitz.com/files/angular-ivy-mleyrm/github/tonykobe8/angular-ivy-mleyrm/master/download.jpeg"},
+  todolist = [{name:"Matome",Sname:"kobe",age:"30",Tittle:"system admin",button:"",image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/download.jpeg"},
+  {name:"hhhhnda",Sname:"ghhhh",age:"20",Tittle:"Web designer",button:"",image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/download (1).jpeg"},
+   {name:"ngghdajh",Sname:"gg",age:"40",Tittle:"system developer",button:"",image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/images (1).jpeg"},
     {name:"hnda",Sname:"ggggg",age:"25",Tittle:"ICT officer",button:""},
      {name:"1mmanhda",Sname:"g",age:"24",Tittle:"IT technician",button:""},
      {name:"2mmavnda",Sname:"kkkk",age:"24",Tittle:"software developer",button:""},
      {name:"3mmandbna",Sname:"nnnnn",age:"24",Tittle:"Software tester",button:""},
      {name:"4mmhhhanda",Sname:"ppppp",age:"24",Tittle:"IT technician",button:""},
      {name:"5mmahnda",Sname:"uuuu",age:"24",Tittle:"IT security",button:""},
-  {name:"kkkwanda",Sname:"gjjgjjkk",age:"33",Tittle:"system analyst",button:""}];}
+  {name:"kkkwanda",Sname:"gjjgjjkk",age:"33",Tittle:"system analyst",button:"",image:"https://stackblitz.com/files/angular-ivy-pxmohb/github/tonykobe8/angular-ivy-pxmohb/master/images.jpeg"}];}
